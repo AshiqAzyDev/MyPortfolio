6ba1afe7b44a54ee6fe5dbe3ba42b042b2ec8df5
AshiqueBourg <ashique@bourgeontech.com>
2024-11-25 11:00:50 +0530

    /MyPortfolio test 1

--- src/content/information.js
+++ src/content/information.js
@@ -6,7 +6,7 @@ const calculateExperienceYears = (startYear) => {
 };
 
 const userData = {
-  firstName: "<Ashique",
+  firstName: "<Aaaashique",
   lastName: " P Raj/>",
   title: "About Me",
   img: "/MyPortfolio/ash (2).jpg", 
